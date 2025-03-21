2a4a57d5e26f44adb9a2942af9274adaf9e13563
abraham-peter <Peter.St.Abraham@student.utcluj.ro>
2025-03-21 10:07:14 +0200

    changes

---
+++ test.py
@@ -0,0 +1,95 @@
+import random
+from collections import Counter
+lista_cnp1=[1990518123456,
+5020729106789,
+6030315223345,
+1951224089123,
+2970615112234,
+5010206123456,
+2991231223456,
+1960715129876,
+6010928123456,
+5030819234567]
+dict_cnp={}
+prenume_baieti = [
+    "Andrei", "Mihai", "Gabriel", "Florin", "Cristian", "Alexandru", "Radu", 
+    "Stefan", "Ionut", "Vlad", "Bogdan", "Daniel", "Paul", "Victor", "Emil", 
+    "Ciprian", "Razvan", "George", "Lucian", "Doru"
+]
+
+prenume_fete = [
+    "Elena", "Maria", "Ioana", "Andreea", "Cristina", "Raluca", "Diana", "Ana", 
+    "Simona", "Lavinia", "Monica", "Oana", "Bianca", "Georgiana", "Alina", 
+    "Claudia", "Denisa", "Gabriela", "Laura", "Nicoleta"
+]
+
+nume_familie = [
+    "Popescu", "Ionescu", "Dumitrescu", "Stoica", "Radu", "Tudor", "Moldovan", 
+    "Georgescu", "Marinescu", "Lazar", "Enache", "Filip", "Neagu", "Grigorescu", 
+    "Petrescu", "Iliescu", "Serban", "Barbu", "Sandu", "Ciobanu"
+]
+
+# def hash_table_pe_judete(lista_cnp):
+    # hash_table_judet = {i: [] for i in range(1,43)}
+    # for cnp in lista_cnp:
+    #     hash_value_str =str(cnp)
+    #     hash_value_judet=int(hash_value_str[8:10])
+    #     hash_table_judet[hash_value_judet].append(cnp)
+    # return hash_table_judet
+# def test_hash_table_pe_mod1000(lista_cnp):
+#     hash_table_pe_mod1000 = {i: [] for i in range(1000)}
+#     for cnp in lista_cnp:
+#         hash_value = cnp % 1000
+#         hash_table[hash_value].append(cnp)
+#     return hash_table
+     
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+    
+    
+   
