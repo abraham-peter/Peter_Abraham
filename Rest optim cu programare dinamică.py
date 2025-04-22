30006d696377c47998900081adad8addcdb665ed
abraham-peter <abrahaam699@gmail.com>
2025-04-22 17:20:10 +0300

    changes

--- Rest optim cu programare dinamică.py
+++ Rest optim cu programare dinamică.py
@@ -10,9 +10,10 @@ produse= [
     { "nume": "Apa", "pret": 2 },
     { "nume": "Cafea", "pret": 9 }
   ]
-copie_bancnote=bancnote
-registru_clienti={}
 import random
+     
+    
+
 def calculare_rest_dinamic(rest):
   dp=[rest+1]*(rest+1)
   dp[0]=0
@@ -21,36 +22,34 @@ def calculare_rest_dinamic(rest):
           if amount-bancnota["valoare"]>=0:
               dp[amount]=min(dp[amount],1+dp[amount-bancnota["valoare"]])
               bancnota["stoc"]-=1
-  for bancnota,copie_bancnota in zip(bancnote,copie_bancnote):
-      diferenta=copie_bancnota["stoc"]-bancnota["stoc"]-1
-      if diferenta>0:
-          print(f"Numarul de bancnote folosite din  {bancnota['valoare']} este {diferenta}")
   if dp[amount]!=rest+1:
       return dp[amount]
-  
 import random
 if __name__ == "__main__":
     while(True):
-        print("Inturduceti numele clientului:")
-        nume_client=input()
+        print("Inturduceti numele produsului:")
+        nume_produs=input()
         print("Intrduceti suma platita:")
         suma_platita=int(input())
-        rest=calculare_rest_dinamic(suma_platita)
+        for i in range(5):
+            if produse[i]["nume"]==nume_produs:
+                stoc_ramas=bancnote[i]["stoc"]-1
+                bancnote[i]["stoc"]=stoc_ramas
+                if stoc_ramas<0:
+                    print("Nu avem suficiente produse")
+                    break
+                rest=suma_platita-produse[i]["pret"]
+                break
+                
+
+        print("Restul este:",rest)
+        min_bancnote=calculare_rest_dinamic(rest)
         if rest<0:
             print("Nu avem suficiente bani")
         elif rest==0:
             print("Nu este rest de dat")
-        print(nume_client,suma_platita,rest)
-    # rest=suma_platita-produse[0]["pret"]#30-7=23
-    # rest=
-    # print(rest)
-    # if(rest<0):
-    #     print("Nu avem suficiente bani")
-    # elif(rest==0):
-    #     print("Nu este rest de dat")
-    # else:
-    #     print(calculare_rest_dinamic(rest))
-    
+        print(nume_produs,suma_platita,rest,min_bancnote,stoc_ramas)
+        
     
 
 
